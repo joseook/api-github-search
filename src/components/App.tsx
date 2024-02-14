072b49fc8cf7c6fb16a0c6413a388dffcb87c20e
joseook <pauloowneto@gmail.com>
2024-02-14 18:26:33 -0300

    Initial update

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="text-[#0000]">
+        <h1>Github Finder</h1> 
+      </div>
+      <div>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './assets/components/App.tsx'
-import './index.css'
+import App from './components/App.tsx'
+import './global.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
