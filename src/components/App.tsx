6539717712f033c2173931d46e259d5d3cc89fd6
joseook <pauloowneto@gmail.com>
2024-02-15 22:10:37 -0300

    Big update added responsive

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,8 +3,8 @@ import { Outlet } from "react-router-dom";
 function App() {
   return (
     <div className="flex justify-center items-center ">
-      <div className="rounded-lg bg-gradient-to-r from-blue-400 to-blue-300 p-5 text-center w-[550px] h-[350px]">
-        <h1 className="text-6xl mt-4 font-bold">Github Search</h1>
+      <div className="rounded-lg bg-gradient-to-r from-blue-400 to-blue-300 xl:p-5 lg:p-5 md:p-4 text-center xl:lg:w-[550px] xl:h-[350px] w-[400px] p-4">
+        <h1 className="xl:text-6xl lg:text-6xl md:text-5xl text-4xl mt-4 font-bold">Github Search</h1>
         <Outlet />
       </div>
     </div>
